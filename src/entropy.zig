54bd4ac69e6a9af357f4303b792d8ebfbab37d2f
Arturo Albacete <aalbacetef@gmail.com>
2024-02-02 11:04:19 +0100

    clean up code; add tests

--- src/entropy.zig
+++ src/entropy.zig
@@ -1,19 +1,14 @@
 const std = @import("std");
-const testing = std.testing;
 
 const bits = 8;
 const max_size = 1 << bits;
 
 pub const Histogram = struct {
     total: u64 = 0,
-    data: [max_size]u64 = undefined,
+    data: [max_size]u64,
 
     pub fn init() Histogram {
-        var h = Histogram{};
-
-        @memset(&h.data, 0);
-
-        return h;
+        return .{ .data = [_]u64{0} ** max_size };
     }
 
     pub fn munch(self: *Histogram, bytes: []u8) void {
@@ -24,6 +19,8 @@ pub const Histogram = struct {
     }
 };
 
+const log2bytes = 8.0;
+
 pub fn calculate(h: Histogram) f64 {
     var entr: f64 = 0;
     const total: f64 = @floatFromInt(h.total);
@@ -32,10 +29,27 @@ pub fn calculate(h: Histogram) f64 {
         if (val == 0) {
             continue;
         }
-
-        const p: f64 = @as(f64, @floatFromInt(val)) / total;
+        const count: f64 = @floatFromInt(val);
+        const p = count / total;
         entr += p * std.math.log2(p);
     }
 
-    return -entr;
+    // @NOTE: avoid -0
+    if (entr == 0.0) {
+        return 0.0;
+    }
+
+    return -entr / log2bytes;
+}
+
+test "it calculates the entropy correctly" {
+    var bytes = [_]u8{ 0x0, 0x01, 0x02, 0x03, 0x04 };
+    var h = Histogram.init();
+    h.munch(&bytes);
+
+    const got = calculate(h);
+    const N: f64 = @floatFromInt(bytes.len);
+    const want: f64 = std.math.log2(N) / log2bytes;
+
+    try std.testing.expectApproxEqRel(want, got, 0.001);
 }
